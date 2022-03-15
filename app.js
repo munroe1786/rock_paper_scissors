749b7fcdc4d20dfca90129e10ac2a31348df494b
munroe1786 <era3munroe@gmail.com>
2022-03-15 18:11:29 -0500

    add possibleChoices forEach function that listens for user clicking on a button / add Display to computerChoice and userChoice variables for clarity

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-const computerChoice = document.getElementById('computer-choice')
-const userChoice = document.getElementById('user-choice')
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+
+let userChoice
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (event) => {
+    userChoice = event.target.id
+}))
