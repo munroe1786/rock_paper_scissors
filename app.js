23c94d83ec5e3e3fa9ff9e40af7dbc5e7b201ec0
munroe1786 <era3munroe@gmail.com>
2022-03-17 21:53:52 -0500

    add generateComputerChoice function to app.js

--- app.js
+++ app.js
@@ -8,4 +8,10 @@ let userChoice
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (event) => {
     userChoice = event.target.id
     userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
 }))
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1// or you can use possibleChoice.length
+    console.log(randomNumber)
+}
